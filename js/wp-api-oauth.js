f764480f88645e2cdbeb2d364dddf95fa5586c28
Adam Silverstein <adam@10up.com>
2017-03-17 14:06:16 -0400

    Add some logging, send headers with requests

--- js/wp-api-oauth.js
+++ js/wp-api-oauth.js
@@ -5,27 +5,30 @@ var wp = window.wp || {};
 wp.oauth = {};
 
 // Set up the oauth connection.
-wp.oauth.setup = function( publicKey, secretKey, requestUrl ) {
+wp.oauth.setup = function( publicKey, secretKey, requestUrl, authorizeUrl ) {
 
 wp.oauth.debug( 'Setup OAuth', requestUrl, [ publicKey, secretKey ] );
 
-// Are we using OAuth 1 and don't already have a token in the response
-if ( wpApiOauthSettings.oauth1 && _.isNull( wpApiOauthSettings.oauth1Token ) ) {
-	oauth = new OAuth( {
-		consumer: {
-			'public': publicKey,
-			'secret': secretKey
-		},
-		signature_method: 'HMAC-SHA1'
+// We don't already have a token in the response.
+if ( _.isNull( wpApiOauthSettings.oauth1Token ) ) {
+	var token,
+		oauth = new OAuth( {
+			consumer: {
+				'public': publicKey,
+				'secret': secretKey
+			},
+			signature_method: 'HMAC-SHA1'
 
-	} );
-	requestData = {
-		url: requestUrl,
-		method: 'POST',
-		data: {
-			oauth_callback: window.location.toString().replace( location.search, '' )
-		}
-	};
+		} );
+		requestData = {
+			url: requestUrl,
+			method: 'POST',
+			data: {
+				oauth_callback: window.location.toString().replace( location.search, '' )
+			}
+		};
+
+	wp.oauth.debug( 'Requesting authorization tokens' );
 
 	// Request the authorization tokens.
 	jQuery.ajax( {
@@ -40,14 +43,14 @@ if ( wpApiOauthSettings.oauth1 && _.isNull( wpApiOauthSettings.oauth1Token ) ) {
 			} );
 		}
 	} ).done( function( tokens ) {
-
-		token = wp.api.utils.extractTokens( tokens, true );
+		wp.oauth.debug( 'Got authorization tokens, storing and redirecting' );
+		token = wp.oauth.extractTokens( tokens, true );
 
 		// Store the returned token data into the session.
 		sessionStorage.setItem( 'tokenPublic', JSON.stringify( token['public'] ) );
 		sessionStorage.setItem( 'tokenSecret', JSON.stringify( token.secret ) );
 
-		window.location.href = wpApiOauthSettings.oauth1.authorize +  '?oauth_token=' + token['public'];
+		window.location.href = authorizeUrl + '?oauth_token=' + token['public'];
 
 	} );
 }
@@ -57,77 +60,99 @@ wp.oauth.debug = function( a = '', b = '', c = '' ) {
 	console.log( 'wp.oauth.debug', a, b, c );
 }
 
+// Extract tokens from the returned token string.
+wp.oauth.extractTokens = function( tokens, hasAdditionalData ) {
+	var tokenPublic, tokenSecret;
+	wp.oauth.debug( 'Extracting tokens' );
+
+	tokenPublic = tokens.substr( tokens.indexOf( 'oauth_token' ) + 'oauth_token'.length + 1, tokens.indexOf( '&', tokens.indexOf( 'oauth_token' ) ) - tokens.indexOf( 'oauth_token' ) - ( 'oauth_token'.length + 1 ) );
+
+	if ( hasAdditionalData ) {
+		tokenSecret = tokens.substr( tokens.indexOf( 'oauth_token_secret' ) + 'oauth_token_secret'.length + 1, tokens.indexOf( '&', tokens.indexOf( 'oauth_token_secret' ) ) - tokens.indexOf( 'oauth_token_secret' ) - (  'oauth_token_secret'.length + 1 ) );
+	} else {
+		tokenSecret = tokens.substr( tokens.indexOf( 'oauth_token_secret' ) + 'oauth_token_secret'.length + 1 );
+	}
+
+	return {
+		'public': tokenPublic,
+		'secret': tokenSecret
+	};
+
+};
+
 // Set up OAuth, using localized or passed credentials.
-wp.oauth.connect = function( publicKey, secretKey, request ) {
+wp.oauth.connect = function( publicKey, secretKey, request, authorize ) {
 	var secretKey = secretKey || wpApiOauthSettings.oauth1Secret,
 		publicKey = publicKey || wpApiOauthSettings.oauth1Public;
 		wp.oauth.debug( 'OAuth Connect' );
 
+	wp.oauth.debug( 'OAuth connecting' );
+
+
 	if ( ! localStorage.getItem( 'wpOathToken' ) ) {
 
 		// Setup the oath process.
-		wp.oauth.setup( publicKey, secretKey, request );
+		wp.oauth.setup( publicKey, secretKey, request, authorize );
 	}
 };
 var originalInit = Backbone.Model.prototype.initialize;
+wpApiOauthSettings.oauth1 = { 'access': 'http://wpdev.localhost/oauth1/access' }
 Backbone.Model.prototype.initialize = function( model, arguments ) {
 
-	if( model ) {
-		// NEXT STEP: Handle the returned temporary OAuth token, requesting a long term token.
-		if ( ( ! _.isNull( wpApiOauthSettings.oauth1Token ) && ( ! localStorage.getItem( 'wpOathToken' ) ) ) ) {
-			wp.oauth.debug( 'Found wpOathToken' );
-			// Construct a new request to get a long term authorization token.
-			token = {
-				'public': JSON.parse( sessionStorage.getItem( 'tokenPublic' ) ),
-				'secret': JSON.parse( sessionStorage.getItem( 'tokenSecret' ) )
-			};
-			oauth = new OAuth( {
-				consumer: {
-					'public': wpApiOauthSettings.oauth1Public,
-					'secret': wpApiOauthSettings.oauth1Secret
-				},
-				signature_method: 'HMAC-SHA1'
+	// NEXT STEP: Handle the returned temporary OAuth token, requesting a long term token.
+	if ( ( ! _.isNull( wpApiOauthSettings.oauth1Token ) && ( ! localStorage.getItem( 'wpOathToken' ) ) ) ) {
+		wp.oauth.debug( 'Found wpOathToken' );
+		// Construct a new request to get a long term authorization token.
+		token = {
+			'public': JSON.parse( sessionStorage.getItem( 'tokenPublic' ) ),
+			'secret': JSON.parse( sessionStorage.getItem( 'tokenSecret' ) )
+		};
+		oauth = new OAuth( {
+			consumer: {
+				'public': wpApiOauthSettings.oauth1Public,
+				'secret': wpApiOauthSettings.oauth1Secret
+			},
+			signature_method: 'HMAC-SHA1'
 
-			} );
-			requestData = {
-				url: wpApiOauthSettings.oauth1.access,
-				method: 'POST',
-				data: {
-					oauth_callback: window.location.toString().replace( location.search, '' ),
-					oauth_verifier: wpApiOauthSettings.oauth1Verifier,
-					oauth_token:    wpApiOauthSettings.oauth1Token
-				}
-			};
-			jQuery.ajax( {
-				url: requestData.url,
-				type: requestData.method,
+		} );
+		requestData = {
+			url: wpApiOauthSettings.oauth1.access,
+			method: 'POST',
+			data: {
+				oauth_callback: window.location.toString().replace( location.search, '' ),
+				oauth_verifier: wpApiOauthSettings.oauth1Verifier,
+				oauth_token:    wpApiOauthSettings.oauth1Token
+			}
+		};
+		wp.oauth.debug( 'Requesting long term token' );
+		jQuery.ajax( {
+			url: requestData.url,
+			type: requestData.method,
 
-				// Add the authorization headers.
-				beforeSend: function( xhr ) {
-					var oauthHeaders = oauth.toHeader( oauth.authorize( requestData, token ) );
-					_.each( oauthHeaders, function( header, index ) {
-						xhr.setRequestHeader( index, header );
-					} );
-				}
-			} ).success( function( tokens ) {
+			// Add the authorization headers.
+			beforeSend: function( xhr ) {
+				var oauthHeaders = oauth.toHeader( oauth.authorize( requestData, token ) );
+				_.each( oauthHeaders, function( header, index ) {
+					xhr.setRequestHeader( index, header );
+				} );
+			}
+		} ).success( function( tokens ) {
 
-				// We have a valid token, store it in localStorage.
-				token = wp.api.utils.extractTokens( tokens, false );
-				localStorage.setItem( 'wpOathToken', JSON.stringify( token ) );
+			wp.oauth.debug( 'Got long term token, storing in localStorage' );
 
-			} );
+			// We have a valid token, store it in localStorage.
+			token = wp.oauth.extractTokens( tokens, false );
+			localStorage.setItem( 'wpOathToken', JSON.stringify( token ) );
 
-		} else {
-			wp.oauth.debug( 'Didn\'t find OAuth token' );
-		}
+		} );
+
+	} else {
+		wp.oauth.debug( 'Didn\'t find OAuth token' );
 	}
+
 	originalInit.apply( model, arguments );
 
 }
-jQuery( document ).ready( function() {
-wp.api.WPApiBaseModel = wp.api.WPApiBaseModel.extend(
-/** @lends WPApiBaseModel.prototype  */
-{
 	/**
 	 * Set nonce header before every Backbone sync.
 	 *
@@ -136,70 +161,68 @@ wp.api.WPApiBaseModel = wp.api.WPApiBaseModel.extend(
 	 * @param {{beforeSend}, *} options.
 	 * @returns {*}.
 	 */
-	sync: function( method, model, options ) {
-		var beforeSend, token, oauth, requestData;
+wp.api.WPApiBaseModel.prototype.sync = function( method, model, options ) {
+	var beforeSend, token, oauth, requestData;
 
-		options = options || {};
+	options = options || {};
 
-		// Remove date_gmt if null.
-		if ( _.isNull( model.get( 'date_gmt' ) ) ) {
-			model.unset( 'date_gmt' );
-		}
+	// Remove date_gmt if null.
+	if ( _.isNull( model.get( 'date_gmt' ) ) ) {
+		model.unset( 'date_gmt' );
+	}
 
-		// Remove slug if empty.
-		if ( _.isEmpty( model.get( 'slug' ) ) ) {
-			model.unset( 'slug' );
-		}
+	// Remove slug if empty.
+	if ( _.isEmpty( model.get( 'slug' ) ) ) {
+		model.unset( 'slug' );
+	}
 
-		// Use OAuth authentication if available.
-		token = JSON.parse( localStorage.getItem( 'wpOathToken' ) );
-		if ( ! _.isUndefined( token ) ) {
+	// Use OAuth authentication if available.
+	token = JSON.parse( localStorage.getItem( 'wpOathToken' ) );
+	if ( ! _.isUndefined( token ) ) {
 
-			oauth = new OAuth( {
-				consumer: {
-					'public': wpApiOauthSettings.oauth1Public,
-					'secret': wpApiOauthSettings.oauth1Secret
-				},
-				signature_method: 'HMAC-SHA1'
+		oauth = new OAuth( {
+			consumer: {
+				'public': wpApiOauthSettings.oauth1Public,
+				'secret': wpApiOauthSettings.oauth1Secret
+			},
+			signature_method: 'HMAC-SHA1'
+		} );
+
+		options.beforeSend = function( xhr, request ) {
+
+			requestData = {
+				url: request.url,
+				method: request.type
+			};
+			_.each( oauth.toHeader( oauth.authorize( requestData, token ) ), function( header, index ) {
+				xhr.setRequestHeader( index, header );
 			} );
 
-			options.beforeSend = function( xhr, request ) {
+		};
 
-				requestData = {
-					url: request.url,
-					method: request.type
-				};
-				_.each( oauth.toHeader( oauth.authorize( requestData, token ) ), function( header, index ) {
-					xhr.setRequestHeader( index, header );
-				} );
+		// When using OAuth, turn off nonce/cookie authentication.
+		delete wpApiSettings.nonce;
 
-			};
-
-			// When using OAuth, turn off nonce/cookie authentication.
-			delete wpApiSettings.nonce;
-
-		}
-
-		if ( ! _.isUndefined( wpApiSettings.nonce ) && ! _.isNull( wpApiSettings.nonce ) ) {
-			beforeSend = options.beforeSend;
-
-			// @todo enable option for jsonp endpoints
-			// options.dataType = 'jsonp';
-
-			options.beforeSend = function( xhr ) {
-				xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
-
-				if ( beforeSend ) {
-					return beforeSend.apply( this, arguments );
-				}
-			};
-		}
-
-		// Add '?force=true' to use delete method when required.
-		if ( this.requireForceForDelete && 'delete' === method ) {
-			model.url = model.url() + '?force=true';
-		}
-		return Backbone.sync( method, model, options );
 	}
-} );
-} );
+
+	if ( ! _.isUndefined( wpApiSettings.nonce ) && ! _.isNull( wpApiSettings.nonce ) ) {
+		beforeSend = options.beforeSend;
+
+		// @todo enable option for jsonp endpoints
+		// options.dataType = 'jsonp';
+
+		options.beforeSend = function( xhr ) {
+			xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+
+			if ( beforeSend ) {
+				return beforeSend.apply( this, arguments );
+			}
+		};
+	}
+
+	// Add '?force=true' to use delete method when required.
+	if ( this.requireForceForDelete && 'delete' === method ) {
+		model.url = model.url() + '?force=true';
+	}
+	return Backbone.sync( method, model, options );
+}
